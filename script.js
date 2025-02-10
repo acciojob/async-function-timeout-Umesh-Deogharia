ebca222de7bb97761134b72e079925df070b41ac
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2025-02-10 22:33:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+const output = document.querySelector("#output");
+const btn = document.querySelector("#btn");
+const text = document.querySelector("#text");
+const delay = document.querySelector("#delay");
+
+// const name = prompt("What is your name");
+// 	  const delay = prompt("What delay do you want");
+btn.addEventListener("submit", async()=>{
+	output.innerText="";
+	
+})
+
+if () {
+	
+}
+// 	prompt("");
+// 	prompt("");
